be52b410c6a09950417d5f7abe5a42d59a7d5a55
whitearsenic <37941642+whitearsenic@users.noreply.github.com>
2019-12-13 20:44:53 +0200

    dfsads

--- index.php
+++ index.php
@@ -1,23 +1,8 @@
 <?php
 session_start();
 error_reporting(0);
-include('includes/config.php');
-if(isset($_GET['action']) && $_GET['action']=="add"){
-	$id=intval($_GET['id']);
-	if(isset($_SESSION['cart'][$id])){
-		$_SESSION['cart'][$id]['quantity']++;
-	}else{
-		$sql_p="SELECT * FROM products WHERE id={$id}";
-		$query_p=mysqli_query($con,$sql_p);
-		if(mysqli_num_rows($query_p)!=0){
-			$row_p=mysqli_fetch_array($query_p);
-			$_SESSION['cart'][$row_p['id']]=array("quantity" => 1, "price" => $row_p['productPrice']);
-			header('location:index.php');
-		}else{
-			$message="Product ID is invalid";
-		}
-	}
-}
+include('C_fetchData.php');
+
 
 ?>
 
