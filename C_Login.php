80de95f3ed3735821238a98173a3dd901f94ab2d
eladham2020 <adham.marey4@yhoo.com>
2019-12-13 21:00:13 +0200

    merge

--- C_Login.php
+++ C_Login.php
@@ -3,6 +3,23 @@ session_start();
 error_reporting(0);
 require_once 'includes/config.php';
 
+
+if(!empty($_POST["email"])) {
+	$email= $_POST["email"];
+
+		$result =mysqli_query($con,"SELECT  email FROM  users WHERE  email='$email'");
+		$count=mysqli_num_rows($result);
+if($count>0)
+{
+echo "<span style='color:red'> Email already exists .</span>";
+ echo "<script>$('#submit').prop('disabled',true);</script>";
+} else{
+
+	echo "<span style='color:green'> Email available for Registration .</span>";
+ echo "<script>$('#submit').prop('disabled',false);</script>";
+}
+}
+
 // Code user Registration
 if(isset($_POST['change']))
 {
@@ -81,6 +98,8 @@ $_SESSION['errmsg']="Invalid email id or Password";
 exit();
 }
 }
+
+
 
 
 ?>
